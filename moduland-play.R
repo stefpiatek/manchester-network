ee3f61388fb1b5e1c996cee4996603b2a804b640
Stefan Piatek <stefan.piatek@nhs.net>
2018-12-06 16:55:56 +0000

    Tidy plot and add HPO correlation

--- moduland-play.R
+++ moduland-play.R
@@ -1,6 +1,9 @@
 library(tidyverse)
 library(here)
 library(ggrepel)
+library(VarfromPDB)
+library(RISmed)
+library(stringi)
 
 ### Set custom theme ----
 
@@ -19,9 +22,15 @@ theme_custom <- function (base_size = 12, base_family = "") {
 
 theme_set(theme_custom())
 
-palette_18 <- c("#000000","#4286ff","#b3a80a","#D3D3D3","#6e0068","#01dca1","#f44575","#005aac","#f58037","#003888","#ff8868","#c492ff","#884600","#60004c","#d48267","#ff7ea9","#808080", "#5fe17a")
+palette_5 <- c( "#16b616", "#A3A3A3", "#B80000", "#FFA347", "#142952")
 
 ### Moduland analysis ----
+
+panelapp_green_genes <- read_delim(here("data", "BWS_genes-panelapp.txt"), delim = "\t") %>%
+  filter(status == "green")
+
+panelapp_red_genes <- read_delim(here("data", "BWS_genes-panelapp.txt"), delim = "\t") %>%
+  filter(status == "red")
 
 data <- read_csv(here("data", "BIOGRID-ORGANISM-Homo_sapiens-3.4.158.tab2.txt-0-bridgeness.csv")) %>%
   inner_join(read_csv(here("data", "BIOGRID-ORGANISM-Homo_sapiens-3.4.158.tab2.txt-0-linkland.csv"))) %>%
@@ -31,29 +40,76 @@ data <- read_csv(here("data", "BIOGRID-ORGANISM-Homo_sapiens-3.4.158.tab2.txt-0-
   mutate(second_group = if_else(condition = `ModuLand community centrality` < 700 & `ModuLand community centrality` > 200 & `ModuLand bridgeness` > 2,
                                 true = nodeID,
                                 false = "")) %>%
-  mutate(gene_of_interest = paste(first_group, second_group, sep="")) %>%
-  select(-ends_with("_group"))
+  mutate(third_group = if_else(condition = nodeID == "HRAS",
+                                true = nodeID,
+                                false = "")) %>%
+  mutate(gene_of_interest = paste(first_group, second_group, third_group, sep="")) %>%
+  mutate(gene_selected = if_else(condition = gene_of_interest != "",
+                                 true = "selected",
+                                 false = "")) %>%
+  select(-ends_with("_group")) %>%
+  mutate(green_genes = if_else(condition = nodeID %in% panelapp_green_genes$`Gene Symbol`,
+                               true = "green",
+                               false = "")) %>%
+  mutate(red_genes = if_else(condition = nodeID %in% panelapp_red_genes$`Gene Symbol`,
+                             true = "red",
+                             false = "")) %>%
+  mutate(panel_app_status = if_else(condition = green_genes == "" & red_genes == "",
+                                    true = "not assigned",
+                                    false = paste(red_genes, green_genes, sep=""))) %>%
+  select(-ends_with("_genes"))
 
 ggplot(data, aes(x = `ModuLand community centrality`, y = `ModuLand bridgeness`)) +
-  geom_point() +
+  geom_smooth(method='lm', formula= y ~ x) +
+  geom_point(aes(colour=panel_app_status, shape = gene_selected), size = 2.2) +
   scale_x_log10() +
   scale_y_log10() +
   annotation_logticks(base = 10, sides = "bl") +
-  geom_smooth(method='lm', formula= y ~ x) +
-  geom_label_repel(aes(label = paste(gene_of_interest)),
+  geom_label_repel(aes(label = gene_of_interest, fill = panel_app_status),
                    box.padding = 0.35, 
                    point.padding = 0.5,
-                   segment.color = 'grey50') 
+                   alpha=0.8,  
+                   segment.color = 'grey50',
+                   nudge_x = 0.2) +
+  scale_color_manual(values = palette_5) +
+  scale_fill_manual(values = palette_5) +
+  guides(shape = FALSE)
 
 
-data %>%
-  filter(`ModuLand community centrality` > 5000) %>%
-  filter(`ModuLand bridgeness` > 1)
 
-data %>%
-  filter(`ModuLand community centrality` < 700) %>%
-  filter(`ModuLand community centrality` > 200) %>%
-  filter(`ModuLand bridgeness` > 2)
+#### gene phenotype correlation -----
 
-data %>% 
-  filter(nodeID == "IGF2")
+
+
+gene_pheno_matrix <- read_csv(here("data", "gene_pheno_matrix.csv")) %>%
+  as.data.frame()
+row.names(gene_pheno_matrix) <- gene_pheno_matrix$X1
+
+gene_pheno_matrix <- gene_pheno_matrix[, 2:ncol(gene_pheno_matrix)]
+gene_pheno_matrix <- as.matrix(gene_pheno_matrix)
+
+matrix <- (gene_pheno_matrix %*% t(gene_pheno_matrix)) -1
+
+library(gplots)
+heatmap.2(matrix,trace="none")
+
+hc.rows<-hclust(dist(t(matrix)))
+h<-4
+plot(hc.rows,cex=0.5)
+ct<-cutree(hc.rows,h)
+rect.hclust(hc.rows,h)
+
+genes<-names(ct[which(ct==4|ct==3)])
+important_gene_pheno <- gene_pheno_matrix[match(genes,rownames(gene_pheno_matrix)),]
+important_gene_pheno <- important_gene_pheno[, colSums(important_gene_pheno) != 0]
+
+rowSums(important_gene_pheno)
+
+phenotypes <- read_delim(here("data", "ALL_SOURCES_ALL_FREQUENCIES_phenotype_to_genes.txt"), delim= "\t", comment = "#",
+                         col_names = c("HPO", "term_name", "gene_id", "gene_name"))
+
+phenotypes %>%
+  select(HPO, term_name) %>%
+  distinct() %>%
+  filter(HPO %in% colnames(important_gene_pheno)) %>%
+  as.data.frame()
